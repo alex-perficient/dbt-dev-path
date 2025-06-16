ae606a7db1b9952cda33205feefc32a6a6b6016b
Alejandro Pedrero Montiel <alejandro.pedrero@perficient.com>
2025-06-16 16:34:37 +0000

    Materialization Fundamentals iniciando

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -9,15 +9,14 @@ source as (
 transformed as (
 
   select
-
     id as payment_id,
     orderid as order_id,
-    created as payment_created_at,
     paymentmethod as payment_method,
     status as payment_status,
-    round(amount / 100.0, 2) as payment_amount
-
-  from source
+    -- amount is stored in cents, convert it to dollars
+    {{ cents_to_dollars('amount', 4) }} as payment_amount,
+    created as payment_created_at
+from source
 
 )
 
